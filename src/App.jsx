d718dfc92f8126878c32b8b3c2ac4dd96b9760a1
akgbusinessintelligence-pixel <akgbusinessintelligence@gmail.com>
2026-03-08 22:11:57 -0400

    Fix /2026 deployment base path and router basename

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import AdminEditItem from './pages/admin/AdminEditItem.jsx';
 
 function App() {
     return (
-        <Router>
+        <Router basename={import.meta.env.BASE_URL}>
             <div className="app-wrapper">
                 <Routes>
                     {/* Main Site Routes */}
